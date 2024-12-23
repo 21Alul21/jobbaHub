80faa959ceba9758177c4da091a31d9a4426fbc3
david nwokedike <miklileza@gmail.com>
2024-12-23 03:27:09 +0100

    added delete post functionality

--- reactfrontend/src/pages/job_post_detail.js
+++ reactfrontend/src/pages/job_post_detail.js
@@ -1,3 +1,4 @@
+import { FaTrash } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
 import Header from "../components/header";
 import { useState, useEffect } from "react";
@@ -6,57 +7,82 @@ import "../components.css";
 import { Link } from "react-router-dom";
 import Footer from "../components/footer";
 
-
-
 const JobPostDetail = () => {
+  const [job, setJob] = useState([]);
+  const [error, setError] = useState("");
+  const { id } = useParams();
+  const date = new Date();
 
-    const [job, setJob] = useState([]);
-    const [error, setError] = useState('');
-    const { id } = useParams();
-    const date = new Date();
+  function handleDeletePost() {
+    axiosInstance
+      .delete(`http://127.0.0.1:8000/api/v1/delete-post/${id}/`)
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
 
-    useEffect(() => {
-        axiosInstance
-          .get(`http://127.0.0.1:8000/api/v1/job-details/${id}/`)
-          .then((response) => {
-            setJob(response.data);
-          })
-          .catch((error) => {
-            setError(error.message);
-          });
-      }, []);
-    
-      if (error) {
-        return (
-          <>
-            <Header />
-            <p>{error}</p>
-          </>
-        );
-      }
-    
+  useEffect(() => {
+    axiosInstance
+      .get(`http://127.0.0.1:8000/api/v1/job-details/${id}/`)
+      .then((response) => {
+        setJob(response.data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+
+  if (error) {
+    return (
+      <>
+        <Header />
+        <p>{error}</p>
+      </>
+    );
+  }
 
   return (
     <>
       <Header />
-      <h2 style={{ marginLeft: "20px", marginTop: "20px" }}>{job.job_title} </h2>
-      <h3 style={{ marginLeft: "20px", marginTop: "20px", color: "green" }}>Job Details</h3>
-      <div
-        className="jobs-post"
-        style={{ padding: "20px" }}
-      >
-        <h3>Job Title: {job.job_title}</h3>
+      <section className="section-job-post_details">
+        <h4>Job Details</h4>
+        <main>
+          <div className="jobs-post">
+            <h2>
+              <strong>Job Title: </strong>
+              {job.job_title}
+            </h2>
 
-        <p style={{ paddingTop: "10px" }}>{job.short_job_description}</p>
-        <p style={{ paddingTop: "10px" }}>{job.full_job_description}</p>
-        <h3 style={{ paddingTop: "5px" }}>Enumeration: {job.enumeration}</h3>
-        <small>posted on {date.toUTCString()}</small>
-      </div>
-      <Link style={{textDecoration: "None", color: "white" }} to={`/job-application/${id}`}><button style={{ cusor: "pointer", }} className="apply-button">Apply</button></Link>
-      < Footer />
+            <p>
+              <strong>Summary:</strong> {job.short_job_description}
+            </p>
+            <p>
+              <strong>Description:</strong>&nbsp;
+              {job.full_job_description}
+            </p>
+            <h3>
+              <strong>Enumeration:</strong>&nbsp;
+              {job.enumeration}
+            </h3>
+            <small>
+              <strong>Posted on:</strong> {date.toUTCString()}
+            </small>
+          </div>
+
+          <div className="btn-cont">
+            <Link to={`/job-application/${id}`}>
+              <button>Apply</button>
+            </Link>
+            <button className="delete" onClick={handleDeletePost}>
+              <FaTrash />
+            </button>
+          </div>
+        </main>
+      </section>
+      <Footer />
     </>
   );
 };
-
 
 export default JobPostDetail;
